ba94568121fb8ddc77daae7d390810e9339c2899
farhanfadhilah <farhanfadhilah87@gmail.com>
2024-12-04 08:54:08 +0700

    first commit

---
+++ app/example_app1/android/app/src/main/kotlin/com/example_app1/example_app1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example_app1.example_app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ core/common_depedencies/lib/example_depedency.dart
@@ -0,0 +1,5 @@
+class ExampleDepedency {
+  final String name;
+  final int age;
+  ExampleDepedency(this.name, this.age);
+}
